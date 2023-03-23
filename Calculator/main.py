bde68961635352d2097d196698327030d27b9e6f
Berat Tezer <64587561+BeratTezer@users.noreply.github.com>
2023-03-23 18:06:37 +0300

    23.3.23 Upload

--- Calculator/main.py
+++ Calculator/main.py
@@ -36,15 +36,16 @@ class Calculator(QtWidgets.QMainWindow):
         self.ui.percentButton.clicked.connect(lambda: self.printScreen(" % "))
         self.ui.commaButton.clicked.connect(lambda: self.printScreen(","))
         
-        # self.ui.equalButton.clicked.connect(self.printScreen)
-        # self.ui.equalButton.clicked.connect(self.equal(self.screenText, 0))
+        self.ui.cButton.clicked.connect(self.clean)
+        self.ui.ceButton.clicked.connect(self.cleanEverything)
+        
+        self.ui.equalButton.clicked.connect(self.equal)
     
     # Functions
-    def equal(self, text, i):
-        # Recursion
-        # if type(text.split(" ")[0]) == 
-        pass
-    
+    def equal(self):
+        self.screenText = eval(str(self.screenText))
+        self.ui.screenLabel.setText(str(self.screenText)[0:13])
+
     def printScreen(self, input):
         cont = 0
         if len(input) == 3 and len(self.screenText) == 1:
@@ -54,32 +55,39 @@ class Calculator(QtWidgets.QMainWindow):
             input = "0,"
         if input in [" * ", " - ", " + ", " / ", " % "] and list(self.screenText)[-1] == " ":
             cont = 1
+        if input == "#":
+            if len(self.screenText) == 0:
+                pass
+            elif list(self.screenText)[-1] == " ":
+                cont = 1
+                self.screenText = self.screenText[0:-3]
+                self.ui.screenLabel.setText(self.screenText)
+            elif list(self.screenText)[-1] in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ","]:
+                if list(self.screenText)[-2] + list(self.screenText)[-1] == "0,":
+                    cont = 1
+                    self.screenText = self.screenText[0:-2]
+                else:
+                    cont = 1
+                    self.screenText = self.screenText[0:-1]
+                self.ui.screenLabel.setText(self.screenText)
+            else:
+                pass
+        if input == "," and self.screenText[-1] == ",":
+            cont = 1
         if cont == 0:
             self.screenText += str(input)
             self.ui.screenLabel.setText(self.screenText)
     
-    def collect(self):
-        pass
-    
-    def extraction(self):
-        pass
-    
-    def multiply(self):
-        pass
-    
-    def divide(self):
-        pass
-    
-    def mod(self):
-        pass
-    
     def clean(self):
-        pass
-    
-    def erase(self):
-        pass
-    
-    
+        if len(self.screenText) > 1:
+            self.screenText = self.screenText[0:(-1-len(self.screenText.split()[-1]))]
+            self.ui.screenLabel.setText(str(self.screenText))
+        else:
+            pass
+        
+    def cleanEverything(self):
+        self.screenText = " "
+        self.ui.screenLabel.setText(self.screenText)
 
 
 def CalculatorWindow():
